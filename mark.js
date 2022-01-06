8f8ea27e8c179509d4fd8860c1dc077cd5b740ec
Mikko Jed Tan <89184826+mjt1400@users.noreply.github.com>
2022-01-07 03:58:47 +0800

    mark js

--- mark.js
+++ mark.js
@@ -10,7 +10,7 @@ function Main(){
 let bttnquote = document.querySelector('button');
 let quote= document.querySelector('p');
 
-let quotes=['"Every no gets me closer to a yes."','"It does not matter how many times you have failed. You only have to be right once."','"Work like there is someone working 24 hours a day to take it all away from you."','"Know your core competencies and focus on being great at them."','"There is always a way to get there"'];
+let quotes=['"Every no gets me closer to a yes."','"It does not matter how many times you have failed. You only have to be right once."','"Work like there is someone working 24 hours a day to take it all away from you."','"Know your core competencies and focus on being great at them."','"There is always a way to get there."'];
 
 bttnquote.addEventListener('click',()=> {
     let index= getRandomNumber(0, quotes.length-1);
